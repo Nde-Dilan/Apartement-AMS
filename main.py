ab96e5d779412b42feb0ac13edbc389fe474c043
Nde Dilane <nde.dilan@ictuniversity.edu.cm>
2023-09-01 17:03:45 +0200

    Adding the login functionality + the the possibility to add a new tenant

--- main.py
+++ main.py
@@ -17,16 +17,6 @@ ui, _ = loadUiType("library.ui")
 light = True
 
 
-# TODO: Create a global variable to handle user and admin, like is_admin, regler le pb avec les combo box et continuer avec la 1ere page
-
-
-def db_manager(self):
-    self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
-    self.cursor = self.db.cursor()
-
-
-
-
 class MainApp(QMainWindow, ui):
     def __init__(self):
         QMainWindow.__init__(self)
@@ -37,52 +27,6 @@ class MainApp(QMainWindow, ui):
         self.show_emails()
         self.show_subjects()
 
-
-    def fill_book_db(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
-        self.cursor = self.db.cursor()
-
-        insert_query = '''
-        INSERT INTO book (title, author, category, available, number_of_pages,description,language)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-        '''
-        book_list = [
-            ("To Kill a Mockingbird", "Harper Lee", "Fiction", 1, 336,
-             "A classic novel tackling issues of race and morality in the American South.", "English"),
-            ("1984", "George Orwell", "Fiction", 1, 328,
-             "A dystopian masterpiece exploring surveillance and totalitarianism.", "English"),
-            ("Pride and Prejudice", "Jane Austen", "Fiction", 1, 432,
-             "A timeless tale of love and social norms in Regency-era England.", "English"),
-            ("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", 1, 180,
-             "A vivid portrayal of the Jazz Age's excesses and the American Dream.", "English"),
-            ("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy", 1, 320,
-             "The magical start to a beloved series about a young wizard's adventures.", "English"),
-            ("The Lord of the Rings", "J.R.R. Tolkien", "Fantasy", 1, 1178,
-             "An epic journey through a world of hobbits, elves, and dark forces.", "English"),
-            ("The Catcher in the Rye", "J.D. Salinger", "Fiction", 1, 277,
-             "A coming-of-age story capturing the disillusionment of a teenage boy.", "English"),
-            ("Animal Farm", "George Orwell", "Fiction", 1, 112,
-             "A satirical allegory illustrating the dangers of political power struggles.", "English"),
-            ("Brave New World", "Aldous Huxley", "Fiction", 1, 288,
-             "A futuristic society explores themes of technology, conformity, and free will.", "English"),
-            ("The Hobbit", "J.R.R. Tolkien", "Fantasy", 1, 310,
-             "A charming adventure featuring a hobbit on a quest for treasure.", "English"),
-            ("The Da Vinci Code", "Dan Brown", "Mystery", 1, 592,
-             "A gripping thriller unraveling cryptic codes and historical secrets.", "English"),
-            ("The Hunger Games", "Suzanne Collins", "Young Adult", 1, 374,
-             "In a dystopian world, a fight for survival unfolds in a televised arena.", "English"),
-            ("The Alchemist", "Paulo Coelho", "Fiction", 1, 208,
-             "A philosophical journey of self-discovery and the pursuit of dreams.", "English"),
-            ("Fahrenheit 451", "Ray Bradbury", "Science Fiction", 1, 249,
-             "In a future society, firemen burn books to control knowledge and ideas.", "English"),
-            ("Gone with the Wind", "Margaret Mitchell", "Fiction", 1, 960,
-             "A sweeping historical romance set against the backdrop of the Civil War.", "English"),
-        ]
-
-        self.cursor.executemany(insert_query, book_list)
-        self.db.commit()
-        print("Data insertion successful.")
-
     def handle_ui_changes(self):
         self.tabWidget.tabBar().setVisible(False)
 
@@ -90,19 +34,14 @@ class MainApp(QMainWindow, ui):
         self.dayBtn.clicked.connect(self.open_day_to_day_tab)
         self.themeBtn.clicked.connect(toggle_theme)
 
-        self.deleteBook.clicked.connect(self.delete_book)
         self.bookBtn.clicked.connect(self.open_books_tab)
         self.userBtn.clicked.connect(self.open_users_tab)
         self.settingBtn.clicked.connect(self.open_settings_tab)
         # operations
-        self.saveBtn.clicked.connect(self.add_new_book)
         self.addUserBtn.clicked.connect(self.add_new_user)
         self.saveChangesUser.clicked.connect(self.update_user)
-        self.searchBtn.clicked.connect(self.search_book)
         self.loginBtn.clicked.connect(self.edit_user)
         self.sendEmailBtn.clicked.connect(self.send_email)
-        # self.quitMailBtn.clicked.connect(self.update_user)
-        self.saveChanges.clicked.connect(self.edit_book)
 
     #####################################################
     ############### opening tabs ######################
@@ -153,125 +92,37 @@ class MainApp(QMainWindow, ui):
             server.login(sender_email, sender_password)
             server.sendmail(sender_email, recipient_email, msg.as_string())
 
-        print("Email sent successfully!")
-
-    #####################################################
-    ############### book operations ######################
-
-    def add_new_book(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
-        self.cursor = self.db.cursor()
-
-        book_title = self.book_title.text()
-        book_author = self.book_author.text()
-        category = self.category_combo.currentText()
-        available = 0 if self.available_combo.currentText() == 'False' else 1
-        pages = int(self.pages.text())
-        description = self.description.toPlainText()
-        language = self.language_combo.currentText()
-
-        self.cursor.execute('''
-            INSERT INTO book (title, author, category, available, number_of_pages, description, language)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-            ''', (book_title, book_author, category, available, pages, description, language))
-        self.db.commit()
-        self.db.close()
-        self.statusBar().showMessage("Book added successfully")
-
-    def search_book(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
-        self.cursor = self.db.cursor()
-
-        book_title = self.title_search.text()
-        self.cursor.execute('''
-        SELECT * FROM book WHERE title = (%s)
-        ''', (book_title,))
-
-        data = self.cursor.fetchall()
-        # print(data)
-        self.pagesEdit.setText(str(data[0][5]))
-        self.titleEdit.setText(data[0][1])
-        self.authorEdit.setText(data[0][2])
-        self.descriptionEdit.setPlainText(data[0][6])
-        self.categoryEdit.addItem(data[0][3])
-        self.categoryEdit.setCurrentText(data[0][3])
-        print("--------> : ",data[0][3],data[0][7])
-        self.language_combo_2.addItem(data[0][7])
-        self.language_combo_2.setCurrentText(data[0][7])
-        self.availableEdit.setCurrentText("True" if data[0][4] == 1 else "False")
-        self.db.commit()
-        self.db.close()
-
-    def edit_book(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
-        self.cursor = self.db.cursor()
-        book_title = self.title_search.text()
-        self.cursor.execute('''
-                SELECT * FROM book WHERE title = (%s)
-                ''', (book_title,))
-
-        data = self.cursor.fetchall()
-
-        update_query = '''
-                    UPDATE book
-                    SET title = %s, author = %s, category = %s,available = %s, number_of_pages = %s, description = %s, language=%s
-                    WHERE title = %s'''
-
-        pages = self.pagesEdit.text()
-
-        title = self.titleEdit.text()
-        author = self.authorEdit.text()
-        description = self.descriptionEdit.toPlainText()
-        category = self.categoryEdit.currentText()
-        language = self.language_combo_2.currentText()
-        print(self.availableEdit.currentText())
-        available = 0 if self.availableEdit.currentText() == 'False' else 1
-
-        self.cursor.execute(update_query, (title, author, category, available, int(pages), description, language,
-                                           data[0][1],))
-        self.db.commit()
-        self.db.close()
-        self.statusBar().showMessage("Book has been updated successfully!")
-
-    def delete_book(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
-        self.cursor = self.db.cursor()
-        book_title = self.title_search.text()
-        delete_query = "DELETE FROM book WHERE title = %s"
-        self.cursor.execute(delete_query, (book_title,))
-
-        self.db.commit()
-        self.db.close()
-        self.statusBar().showMessage("The book has been deleted successfully!")
+        QMessageBox.Information(self, "Info", "Email sent successfully!", QMessageBox.Ok)
 
     #####################################################
     ############### user operations ######################
 
     def add_new_user(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
+        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='apartement')
         self.cursor = self.db.cursor()
 
         username = self.username.text()
         email = self.email.text()
-        password = self.password.text()
-        confirm_password = self.confirmPassword.text()
+        move_in = self.move_in.date().toString("yyyy-MM-dd")
 
-        user = self.cursor.execute('''
-                       SELECT * FROM users WHERE username = (%s)
-                       ''', (username,)).fetchone()
+        tenant = self.cursor.execute('''
+                       SELECT * FROM tenant WHERE name = (%s)
+                       ''', (username,))
+        print(tenant)
 
-        if user:
+        if tenant:
             # open a dialog box to say that the username is already taken
             pass
         else:
-            if username and email and password and confirm_password and password == confirm_password:
+            if username and email :
                 self.cursor.execute('''
-                            INSERT INTO users (username, password, email)
+                            INSERT INTO tenant (name, email, move_in)
                             VALUES (%s, %s, %s)
-                            ''', (username, password, email))
+                            ''', (username, email, move_in))
                 self.db.commit()
                 self.db.close()
-                self.statusBar().showMessage("The user has been added successfully!")
+                QMessageBox.information(self, "Info", "The user has been added successfully!", QMessageBox.Ok)
+
             else:
                 # Open the dialog box with the message
                 print("Please don't create problem")
@@ -283,7 +134,7 @@ class MainApp(QMainWindow, ui):
         self.cursor = self.db.cursor()
 
         self.cursor.execute('''
-          SELECT email FROM users
+          SELECT email FROM Tenant
           ''')
 
         data = self.cursor.fetchall()
@@ -311,51 +162,51 @@ class MainApp(QMainWindow, ui):
         self.db.close()
 
     def edit_user(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
+        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='apartement')
         self.cursor = self.db.cursor()
 
         username = self.username2.text()
-        password = self.password2.text()
+        email = self.email_2.text()
 
         self.cursor.execute('''
-                               SELECT * FROM users WHERE username = (%s) AND password= (%s)
-                               ''', (username, password,))
-        user = self.cursor.fetchone()
+                               SELECT * FROM tenant WHERE username = (%s) AND email= (%s)
+                               ''', (username, email,))
+        tenant = self.cursor.fetchone()
         #print(user)
-        if user:
-            change_visibility = [self.username3, self.password3, self.email2, self.confirmPassword2,
+        if tenant:
+            change_visibility = [self.username3, self.email_3, self.move_in_3,
                                  self.saveChangesUser]
 
             for obj in change_visibility:
                 obj.setEnabled(True)
 
-            self.username3.setText(user[1])
-            self.password3.setText(user[2])
-            self.email2.setText(user[3])
-            self.confirmPassword2.setText(user[2])
+            self.username3.setText(tenant[1])
+            self.email_3.setText(tenant[2])
+            self.move_in.setText(tenant[3])
         else:
             print("Please don't create problem")
 
     def update_user(self):
-        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='library')
+        self.db = MySQLdb.connect(host='localhost', user='root', password='123456789', db='apartement')
         self.cursor = self.db.cursor()
 
         new_username = self.username3.text()
-        new_email = self.email2.text()
-        new_password = self.password3.text()
-        new_confirm_password = self.confirmPassword2.text()
+        new_email = self.email_3.text()
+        new_move_in = move_in.date().toString("yyyy-MM-dd")
+        # date.fromString(date_string, "yyyy-MM-dd")
+
 
         username = self.username2.text()
         email = self.email.text()
         update_query = '''UPDATE users
-                          SET username = %s , password= %s, email=%s
+                          SET username = %s , email=%s, move_in= %s,
                           WHERE username = %s'''
 
-        if new_username and new_email and new_password and new_confirm_password:
-            self.cursor.execute(update_query, (new_username, new_password, new_email, username,))
+        if new_username and new_email and new_move_in:
+            self.cursor.execute(update_query, (new_username, new_email, new_move_in, username,))
             self.db.commit()
             self.db.close()
-            change_visibility = [self.username3, self.password3, self.email2, self.confirmPassword2,
+            change_visibility = [self.username3, self.email_3, self.move_in_3,
                                  self.saveChangesUser]
 
             for obj in change_visibility:
